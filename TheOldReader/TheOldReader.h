7086f11bf0fe3bdfe6336311b6ecf72520b44726
dengwu.wangdw <dengwu.wangdw@alibaba-inc.com>
2014-05-10 21:07:46 +0800

    Initial Commit

---
+++ TheOldReader/TheOldReader.h
@@ -0,0 +1,13 @@
+//
+//  TheOldReader.h
+//  TheOldReader
+//
+//  Created by 王登武 on 14-5-10.
+//  Copyright (c) 2014年 Cube. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TheOldReader : NSObject
+
+@end
---
+++ TheOldReader/TheOldReader.m
@@ -0,0 +1,13 @@
+//
+//  TheOldReader.m
+//  TheOldReader
+//
+//  Created by 王登武 on 14-5-10.
+//  Copyright (c) 2014年 Cube. All rights reserved.
+//
+
+#import "TheOldReader.h"
+
+@implementation TheOldReader
+
+@end
