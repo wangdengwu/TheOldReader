da209f5109215f710634c3e6a725fa8467c49e2b
dengwu.wangdw <dengwu.wangdw@alibaba-inc.com>
2014-05-11 11:12:26 +0800

    修改测试
    单元测试

--- TheOldReaderTests/TheOldReaderTests.m
+++ TheOldReaderTests/TheOldReaderTests.m
@@ -11,6 +11,8 @@
 
 @interface TheOldReaderTests : XCTestCase{
     TheOldReaderAPI * api;
+    NSRunLoop* runLoop;
+    __block BOOL run;
 }
 
 @end
@@ -21,23 +23,32 @@
 {
     [super setUp];
     api=[[TheOldReaderAPI alloc]init];
+    
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    runLoop=[NSRunLoop currentRunLoop];
+    run=YES;
+    //加异步等待，防止请求还未返回，进程已经退出
+    while (run) {
+        [runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
 }
 
 - (void)testLogin
 {
+    
     [api doLogin:@"dengwu.wang@gmail.com" password:@"qwer123" callback:^(BOOL isSucess, NSString *authToken) {
         XCTAssertTrue(isSucess, @"测试登录成功");
         XCTAssertNotNil(authToken, @"如果登录成功返回的authToken不能为nil");
         NSLog(@"%@",authToken);
+        CFRunLoopStop([runLoop getCFRunLoop]);
+        run=NO;
     }];
-    //加异步等待，防止请求还未返回，进程已经退出
-    [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:20]];
+    
 }
 
 @end
